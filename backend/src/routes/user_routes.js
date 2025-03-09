c0c79d64dfc739e3120eabacc562a4ac80de5dd4
OonXiangYu <oonxy-wm22@student.tarc.edu.my>
2025-03-09 17:37:41 +0000

    version 2.2
    - connect with thomas db but still have api error

--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -1,22 +1,10 @@
 const express = require("express");
-const { authenticateUser, isAdmin } = require("../middleware/authMiddleware");
-const { registerUser, loginUser } = require("../controllers/user_controllers.js");
+const { registerUser, loginUser } = require ("../controllers/user_controllers");
 
 const router = express.Router();
 
-// Public routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-// Protected routes (only authenticated users can access)
-router.get("/profile", authenticateUser, (req, res) => {
-    res.json({ message: "Welcome to your profile!", user: req.user });
-    console.log("Getting /profile for user: ", req.user);
-});
-
-// Admin route example
-router.get("/admin", authenticateUser, isAdmin, (req, res) => {
-    res.json({ message: "Welcome Admin!" });
-});
-
 module.exports = router;
+
