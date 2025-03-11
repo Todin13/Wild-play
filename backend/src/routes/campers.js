9cf58a623c7fdfb90da37c23ca718634981c2fed
thibautRV <125503955+thibautRV@users.noreply.github.com>
2025-03-11 14:42:14 +0000

    campers administation

--- backend/src/routes/campers.js
+++ backend/src/routes/campers.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const campersController = require('../controllers/campersController'); // Import the controller
+const { authenticateUser, isAdmin } = require('../middlewares/authMiddleware'); // Import middleware
+
 
 router.get('/', campersController.getAllCampers); // Route to fetch all campers
+
+// Add a new camper (Admin only)
+router.post('/', authenticateUser, isAdmin, campersController.addCamper);
+
+// Delete a camper by ID (Admin only)
+router.delete('/:id', authenticateUser, isAdmin, campersController.deleteCamper);
 
 module.exports = router;
