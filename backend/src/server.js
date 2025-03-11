a358c5d2a2b0aa3dd871ca1795c587ca6d92ce89
Todin <112660117+Todin13@users.noreply.github.com>
2025-03-11 15:42:37 +0100

    Merge Register, Login and Auth Midlleware (#2)
    * version 1.0
    
    done some field and the validation, can append to db successfulluy
    
    * version 1.1
    
    - Adding the auto-generate user id (user num)
    
    * version 1.0
    
    done some field and the validation, can append to db successfulluy
    
    * version 1.1
    
    - Adding the auto-generate user id (user num)
    
    * Finishing the merge
    
    * Update
    
    * version 1.2
    
    -add address field, license field.
    - register part should be done just haven't merge with thomas part
    
    * Version 2.1
    
    - Done login and register
    
    * version 2.2
    
    - connect with thomas db but still have api error
    
    * fixing problems
    
    * Adding log for debugging
    
    * Adding hashing functions
    
    * Removing min size for user password as we save hashed password
    
    * Renaming hash to hashUtils and creating jwtutils to generate user token (cookies)
    
    * Adding authentification middleware
    
    * Adding middleware usage
    
    ---------
    
    Co-authored-by: OonXiangYu <oonxy-wm22@student.tarc.edu.my>

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { connectDB } = require("./config/db.js");
+const userRoutes = require("./routes/user_routes.js");
+
+dotenv.config();
+connectDB();
+
+require('dotenv').config({ path: '../.env' });
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
