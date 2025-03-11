5156b99928b80666a558135a3e952b365727fdd2
KIRILL SMIRNOV <lamsquirrel75@gmail.com>
2025-03-11 15:11:47 +0000

    booking struckture update

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { getAllBookings, setBooking } = require("../controllers/bookingControllers");
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // get all bookings for a user
 router.get("/:user_id", authMiddleware, getAllBookings);
