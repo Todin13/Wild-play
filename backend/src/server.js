cc7ae9c0bd471d4c6c5f7e3e924ebdaaffdde7f0
KIRILL SMIRNOV <lamsquirrel75@gmail.com>
2025-03-11 14:26:54 +0000

    booking system sets and get all bookings

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require('dotenv').config({ path: '../.env' });
 const { connectDB } = require("./config/db.js");
-const userRoutes = require("./routes/user_routes.js");
 
-dotenv.config();
 connectDB();
 
-require('dotenv').config({ path: '../.env' });
+const userRoutes = require("./routes/user_routes.js");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // middleware to parse JSON
 
 app.use("/api/users", userRoutes);
+app.use("/api/bookings", bookingRoutes); // booking routes
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
