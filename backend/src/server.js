4f15ebf5f010329687a98be70cb20b3845214012
Thomas ODIN <thomasodin13@gmail.com>
2025-03-14 04:53:39 +0100

    Updating server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const { connectDB } = require("./config/db.js");
+require('dotenv').config({ path: '../.env' });
+const cors = require("cors");
+
+// Import Routes
+const tripRoutes = require("./routes/tripRoutes");
 const userRoutes = require("./routes/user_routes.js");
 
-dotenv.config();
+
 connectDB();
 
-require('dotenv').config({ path: '../.env' });
-
 const app = express();
+connectDB();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/users", userRoutes);
+app.use("/api/trips", tripRoutes);
 
-const PORT = process.env.PORT;
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
