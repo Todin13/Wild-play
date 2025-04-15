21a58a7366f081a1516dd7820167a8587a13adb9
Thomas ODIN <thomasodin13@gmail.com>
2025-04-15 01:31:05 +0000

    Update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,14 @@
-const {heroui} = require('@heroui/theme');
+const { heroui } = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
-],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [heroui()],
-}
-
+};
