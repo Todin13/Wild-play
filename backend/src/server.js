8f5e763f15700b5c72af316c11ad03554d38f571
Thomas ODIN <thomasodin13@gmail.com>
2025-04-19 18:11:51 +0000

    Fix error

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,16 @@ Main file to run the API
 
 */
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./config/db.js");
 
-require('dotenv').config("../.env"); 
+require("dotenv").config("../.env");
 
 // Import Routes
-const searchRoutes = require('./routes/searchRoutes.js');
-const campersRoutes = require('./routes/campersRoutes.js');
-const dealsRoutes = require('./routes/dealsRoutes.js');
+const searchRoutes = require("./routes/searchRoutes.js");
+const campersRoutes = require("./routes/campersRoutes.js");
+const dealsRoutes = require("./routes/dealsRoutes.js");
 const bookingRoutes = require("./routes/bookingRoutes");
 const tripRoutes = require("./routes/tripRoutes");
 const guideRoutes = require("./routes/guideRoutes");
@@ -26,37 +26,38 @@ connectDB();
 
 const app = express();
 
-
 // Middleware
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://wild-play.vercel.app/',
-  'https://wild-play-todin13s-projects.vercel.app/',
-  'https://wild-play-git-userpage-todin13s-projects.vercel.app/',
-  'https://wild-play-git-searchpage-todin13s-projects.vercel.app',
-  'https://wild-play-git-fixing-cors-todin13s-projects.vercel.app/'
+  "http://localhost:5173",
+  "https://wild-play.vercel.app",
+  "https://wild-play-todin13s-projects.vercel.app",
+  "https://wild-play-git-userpage-todin13s-projects.vercel.app",
+  "https://wild-play-git-searchpage-todin13s-projects.vercel.app",
+  "https://wild-play-git-fixing-cors-todin13s-projects.vercel.app",
 ];
 
 // Set up CORS middleware to allow only whitelisted origins and enable cookies
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow access
-    } else {
-      callback(new Error('Not allowed by CORS')); // Reject access
-    }
-  },
-  credentials: true // Allow cookies and session credentials
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow access
+      } else {
+        callback(new Error("Not allowed by CORS")); // Reject access
+      }
+    },
+    credentials: true, // Allow cookies and session credentials
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
 app.use("/help", helpRoute);
 app.use("/api/users", userRoutes);
-app.use('/api/search', searchRoutes);
-app.use('/api/campers', campersRoutes);
-app.use('/api/deals', dealsRoutes);
+app.use("/api/search", searchRoutes);
+app.use("/api/campers", campersRoutes);
+app.use("/api/deals", dealsRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/trips", tripRoutes);
 app.use("/api/guides", guideRoutes);
