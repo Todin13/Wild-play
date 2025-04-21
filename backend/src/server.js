ab864af4b91128b29e0a1ab77e79259bda47acac
Thomas ODIN <thomasodin13@gmail.com>
2025-04-21 20:16:09 +0000

    Update on CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(null, true);
     }
   },
   credentials: true,
