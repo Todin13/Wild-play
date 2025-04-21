fba22015120ce7a974d379e7548e3002e39962f4
thibautRV <125503955+thibautRV@users.noreply.github.com>
2025-04-21 14:56:05 +0100

    api change

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "/api",
   // baseURL: "https://wild-play-api.vercel.app/api",
-  baseURL: "https://wild-play-api-git-userpage-todin13s-projects.vercel.app/api",
+  //baseURL: "https://wild-play-api-git-userpage-todin13s-projects.vercel.app/api",
   baseURL: "https://wild-play-api-git-search-campervans-deals-todin13s-projects.vercel.app/api",
   //baseURL: "http://localhost:5019/api",
   withCredentials: true,
