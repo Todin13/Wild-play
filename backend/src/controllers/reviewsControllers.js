7b3b60a259ed77ede0db98037391d6ec40d49be6
Thomas ODIN <thomasodin13@gmail.com>
2025-04-23 16:37:47 +0100

    more logs

--- backend/src/controllers/reviewsControllers.js
+++ backend/src/controllers/reviewsControllers.js
@@ -79,7 +79,7 @@ exports.createGuideReview = async (req, res) => {
       .status(201)
       .json({ message: "Guide review added successfully", review: newReview });
   } catch (error) {
-    console.log(req.user, error)
+    console.log(req.user, id, error)
     res
       .status(500)
       .json({ message: "Error creating guide review", error: error.message });
