afeb9c4d0513dfb681e91a09bed3a50fc7c8f252
KIRILL SMIRNOV <lamsquirrel75@gmail.com>
2025-05-09 18:55:12 +0100

    stripe fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API = axios.create({
   //baseURL: "http://localhost:5019/api",
-  baseURL: "https://wild-play-api.vercel.app/api",
+  baseURL: "https://wild-play-api-git-bookingsfix-todin13s-projects.vercel.app/api",
   withCredentials: true,
 });
 
