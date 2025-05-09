6193d92a335baeae8c40871b887c053bb11d26d0
KIRILL SMIRNOV <lamsquirrel75@gmail.com>
2025-05-09 21:25:42 +0100

    stripe fix

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -42,8 +42,8 @@ router.post('/create-session', async (req, res) => {
         },
         quantity: 1,
       }],
-      success_url: `${req.headers.origin || 'https://wild-play-api.vercel.app/api'}/bookings/${bookingId}`,
-      cancel_url: `https://wild-play-git-bookingsfix-todin13s-projects.vercel.app/api/bookings/${bookingId}`,
+      success_url: `https://wild-play-git-bookingsfix-todin13s-projects.vercel.app/bookings/${bookingId}`,
+      cancel_url: `https://wild-play-git-bookingsfix-todin13s-projects.vercel.app/bookings/${bookingId}`,
       client_reference_id: bookingId.toString(),
     });
 
